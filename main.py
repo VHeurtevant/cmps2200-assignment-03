e28540ae447ae3af5d1811e44ab4c2fab08ed70b
VHeurtevant <murasa.chan@gmail.com>
2025-11-08 15:20:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,71 @@ def med_top_down(S, T, MED={}):
     return MED[(S, T)]
     
 def fast_MED(S, T):
-    # TODO -  implement top-down memoization
-    pass
+    s = len(S)
+    t = len(T)
 
+    dp = [[0]*(t+1) for _ in range(s+1)] # Intializes Array
+
+    for i in range(s+1): #Base cases.
+        dp[i][0] = i  
+    for j in range(t+1):
+        dp[0][j] = j   
+        
+    for i in range(1, s+1):
+        for j in range(1, t+1):
+            if S[i-1] == T[j-1]:       # Characters match, no change needed
+                dp[i][j] = dp[i-1][j-1]
+            else:                       # Checks previous subproblems to see if inserting or deleting is better
+                dp[i][j] = 1 + min(
+                    dp[i][j-1],    # insert
+                    dp[i-1][j]     # delete
+                )
+    return (dp[s][t]) #The optimum solution
+    
 def fast_align_MED(S, T):
-    # TODO - keep track of alignment
-    pass
+
+    #Start by building table
+    s = len(S)
+    t = len(T)
+
+    dp = [[0]*(t+1) for _ in range(s+1)] # Intializes Array
+
+    for i in range(s+1): #Base cases.
+        dp[i][0] = i  
+    for j in range(t+1):
+        dp[0][j] = j   
+        
+    for i in range(1, s+1):
+        for j in range(1, t+1):
+            if S[i-1] == T[j-1]:       # Characters match, no change needed
+                dp[i][j] = dp[i-1][j-1]
+            else:                       # Checks previous subproblems to see if inserting or deleting is better
+                dp[i][j] = 1 + min(
+                    dp[i][j-1],    # insert
+                    dp[i-1][j]     # delete
+                )
+
+    aligned_S = []
+    aligned_T = []
+    i, j = s, t
+    #Start with optimum solution and backtrack 
+    while i > 0 or j > 0:
+        # Match
+        if i > 0 and j > 0 and S[i-1] == T[j-1]:
+            aligned_S.append(S[i-1])
+            aligned_T.append(T[j-1])
+            i -= 1
+            j -= 1
+        # Prefer insertion into S (dash in S) in case of tie
+        elif j > 0 and (i == 0 or dp[i][j-1] <= dp[i-1][j]):
+            aligned_S.append('-')
+            aligned_T.append(T[j-1])
+            j -= 1
+        else:
+            # Otherwise deletion from S is optimal (dash in T)
+            aligned_S.append(S[i-1])
+            aligned_T.append('-')
+            i -= 1
+    
+    return ''.join(reversed(aligned_S)), ''.join(reversed(aligned_T)) #Reverses (built backwards) and concatenates into string
 
